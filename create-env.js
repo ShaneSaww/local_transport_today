53e5b68a48a1a78c6c5c73d98516a76898bdf82d
Jonathan Ingram <jonathan_ingram@hotmail.com>
2020-05-09 23:41:23 +0100

    Editorial preview on Netlify CMS

--- create-env.js
+++ create-env.js
@@ -1,7 +1,4 @@
 const fs = require('fs')
 fs.writeFileSync('./.env', `
-URL=${process.env.URL}\n
-CMS_URL=${process.env.CMS_URL}\n
-READ_EDITORIALS_PATH=${process.env.READ_EDITORIALS_PATH}\n
-READ_EDITORIALS_TOKEN=${process.env.READ_EDITORIALS_TOKEN}
+URL=${process.env.URL}
 `)
--- layouts/default.vue
+++ layouts/default.vue
@@ -31,17 +31,6 @@
     color: #ffffff;
   }
 
-  /*.expand-enter-active,
-  .expand-leave-active {
-    transition: height 1s ease-in-out;
-    overflow: hidden;
-  }
-
-  .expand-enter,
-  .expand-leave-to {
-    height: 0;
-  }*/
-
   .leaflet-grab {
     cursor: move !important; /* much cleaner than Window's cursor verson of 'grab' */
   }
@@ -122,54 +111,5 @@ export default {
       ]
     }
   }
-
-  //name: 'TransitionExpand',
-  /*methods: {
-    enter(element) {
-      const width = getComputedStyle(element).width;
-
-      element.style.width = width;
-      element.style.position = 'absolute';
-      element.style.visibility = 'hidden';
-      element.style.height = 'auto';
-
-      const height = getComputedStyle(element).height;
-
-      element.style.width = null;
-      element.style.position = null;
-      element.style.visibility = null;
-      element.style.height = 0;
-
-      // Force repaint to make sure the
-      // animation is triggered correctly.
-      getComputedStyle(element).height;
-
-      // Trigger the animation.
-      // We use `setTimeout` because we need
-      // to make sure the browser has finished
-      // painting after setting the `height`
-      // to `0` in the line above.
-      setTimeout(() => {
-        element.style.height = height;
-      });
-    },
-    afterEnter(element) {
-      element.style.height = 'auto';
-    },
-    leave(element) {
-      const height = getComputedStyle(element).height;
-
-      element.style.height = height;
-
-      // Force repaint to make sure the
-      // animation is triggered correctly.
-      getComputedStyle(element).height;
-
-      setTimeout(() => {
-        element.style.height = 0;
-      });
-    }
-  },*/
-
 }
 </script>
