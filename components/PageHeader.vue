3ca1b439fd81cb2084795e4152b0c3479aa1c6a0
Jonathan Ingram <jonathan_ingram@hotmail.com>
2020-06-21 21:57:07 +0100

    Fix search button for iOS on keyboard v2

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -40,9 +40,9 @@
             <label for="search" class="sr-only">Search</label>
             <input class="w-full lg:w-auto font-sans border ltt-headline h-10 px-5 pr-12 rounded-lg text-sm focus:outline-none"
               list="search_list" type="search" name="search" id="search" placeholder="Search" :value="searchTerm" autocomplete="off" aria-required="true" required>
-            <input type="submit" value="Submit" aria-hidden="true" class="sr-only" />
-            <button type="submit" class="absolute right-0 top-0 mt-3 mr-4">
-              <svg class="ltt-headline h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
+            <input type="submit" value="Submit" class="sr-only" />
+            <button aria-hidden="true" type="submit" class="absolute right-0 top-0 mt-3 mr-4">
+              <svg focusable="false" class="ltt-headline h-4 w-4 fill-current" xmlns="http://www.w3.org/2000/svg"
                 xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px"
                 viewBox="0 0 56.966 56.966" style="enable-background:new 0 0 56.966 56.966;" xml:space="preserve"
                 width="512px" height="512px">
