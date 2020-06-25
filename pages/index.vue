7a1a958d7afea913fa3004bdafbd5e9bc0347026
Jonathan Ingram <jonathan_ingram@hotmail.com>
2020-06-25 02:25:32 +0100

    Update homepage video with YouTube icon

--- pages/index.vue
+++ pages/index.vue
@@ -11,7 +11,8 @@
               <p class="font-serif mt-2 text-left ltt-text-gray font-normal leading-normal text-sm sm:text-base md:text-sm"><time datetime="2020-06-19" aria-label="19 June 2020">19 Jun 2020</time></p>
           </div>
           <div class="px-6 pb-6 lg:px-0 lg:pb-0">
-            <p class="mx-auto font-sans text-center leading-tight max-width-90p lg:max-w-none text-sm lg:text-left"><a href="https://www.transportxtra.com/tx-events/?id=2468" class="red-block block lg:inline-block">Join the discussion</a></p>
+            <p class="mx-auto font-sans text-center leading-tight max-width-90p lg:max-w-none text-sm lg:text-left"><a href="https://www.youtube.com/embed/dHV_GJJl9X4?autoplay=1" class="red-block flex lg:inline-flex flex-row items-center"><svg aria-hidden="true" focusable="false" class="fill-current w-4 h-4 mr-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M23.495 6.205a3.007 3.007 0 0 0-2.088-2.088c-1.87-.501-9.396-.501-9.396-.501s-7.507-.01-9.396.501A3.007 3.007 0 0 0 .527 6.205a31.247 31.247 0 0 0-.522 5.805 31.247 31.247 0 0 0 .522 5.783 3.007 3.007 0 0 0 2.088 2.088c1.868.502 9.396.502 9.396.502s7.506 0 9.396-.502a3.007 3.007 0 0 0 2.088-2.088 31.247 31.247 0 0 0 .5-5.783 31.247 31.247 0 0 0-.5-5.805zM9.609 15.601V8.408l6.264 3.602z"/></svg>
+            <span>Watch Discussion</span></a></p>
           </div>
         </aside>
         <!--<aside role="complementary" class="homepage-alert w-full mt-4 lg:mt-0 mb-4">
